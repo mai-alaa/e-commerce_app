201ba86caa4175a3bb449cddb54861b55e3a0be6
Mai Alaa <maia28126@gmail.com>
2024-01-30 11:09:51 +0200

    itegrate app with firease

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:ecommerce_app/Features/Splash/Presentation/Views/Splash_screen.dart';
+import 'package:ecommerce_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+void main() async {
   runApp(const ECommerceApp());
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class ECommerceApp extends StatelessWidget {
